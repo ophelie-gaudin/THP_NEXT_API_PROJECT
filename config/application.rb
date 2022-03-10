266284325b476903ea7ca81f818f904e5398fcaa
Istarengwa <muller.lilian@outlook.fr>
2022-03-10 18:20:59 +0100

    add mailjet

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module DeviseVue
     config.middleware.use ActionDispatch::Cookies
 
     config.middleware.use config.session_store, config.session_options
+    #config.middleware.use ActionDispatch::Flash
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
